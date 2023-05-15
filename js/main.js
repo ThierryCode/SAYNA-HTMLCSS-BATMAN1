c8742380a19c1697360f6a9635cccc89134dccc0
KAMAMY Andry Thierry <tchithierry@gmail.com>
2023-05-15 11:19:51 +0300

    Modification sur les icones latérales

--- js/main.js
+++ js/main.js
@@ -1,25 +1,9 @@
-// const AllitemNav = document.querySelectorAll(".itemNav");
-// const link = document.querySelectorAll(".itemNav");
-// link.forEach((item) => {
+// const allItemNav = document.querySelectorAll(".itemNav");
+// allItemNav.forEach((item) => {
 //   item.addEventListener("click", () => {
-//     link.classList.add("active");
+//     allItemNav.forEach((item) => {
+//       item.classList.remove("active");
+//     });
+//     item.classList.add("active");
 //   });
 // });
-// const AllitemNav = document.querySelectorAll(".itemNav");
-// const link = document.querySelectorAll(".itemNav");
-// link.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     item.classList.toggle("active");
-//   });
-// });
-const allItemNav = document.querySelectorAll(".itemNav");
-allItemNav.forEach((item) => {
-  item.addEventListener("click", () => {
-    // Supprimer la classe active de tous les éléments
-    allItemNav.forEach((item) => {
-      item.classList.remove("active");
-    });
-    // Ajouter la classe active uniquement à l'élément cliqué
-    item.classList.add("active");
-  });
-});
